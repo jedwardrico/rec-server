4de9c7b109a04237ef18e6abd6a87136578389de
jedwardrico <jacobedwardrico@gmail.com>
2024-04-18 22:34:51 -0700

    add tests and cors

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -19,4 +19,10 @@ describe('AppController', () => {
       expect(appController.getHello()).toBe('Hello World!');
     });
   });
+
+  describe('foodtrucks', () => {
+    it('should return data', () => {
+      expect(appController.getFoodTrucks()).toHaveLength;
+    });
+  });
 });
